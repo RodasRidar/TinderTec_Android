cab796f15010008bf0a38b439d23bf6b7fdb896f
Richard <richard_rodas01@hotmail.com>
2022-06-19 19:22:24 -0500

    Me gustas termiando

--- app/src/main/java/com/example/tindertec/MainActivity.java
+++ app/src/main/java/com/example/tindertec/MainActivity.java
@@ -67,11 +67,12 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case R.id.nav_likes:
-                        LikesFragment likesFragment = new LikesFragment();
+                        Fragment_like likesFragment = new Fragment_like();
                         getSupportFragmentManager().beginTransaction()
                                 .replace(R.id.fragment_container_view, likesFragment)
                                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                                 .addToBackStack(null).commit();
+
                         break;
 
                     case R.id.nav_subs:
